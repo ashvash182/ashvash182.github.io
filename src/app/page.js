0a7b16536131a8575274192135d9d588f02d3629
ashvash182 <anshosca@gmail.com>
2023-08-16 17:46:05 -0700

    fixing eslint issues

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <main class="space-y-4 flex flex-col items-center min-h-screen justify-between p-24">
       <div class='mb-4 image-cropper rounded-2xl'>
-        <Image src= {realsquattedsf} width={300} height={200} class='rounded-full pfp-position'></Image>
+        <Image src= {realsquattedsf} width={300} height={200} class='rounded-full pfp-position' alt='ansh v. profile picture'></Image>
       </div>
       <br></br>
       <div class='space-y-4 max-w-[375px] items-center text-justify'>
@@ -26,15 +26,15 @@ export default function Home() {
           Hey!
         </p>
         <p className='mb-4 text-justify'>
-          I'm Ansh, a software engineer and student based in Berkeley, CA with an interest in deep learning, full-stack engineering, and system design. 
+          I&apos;m Ansh, a software engineer and student based in Berkeley, CA with an interest in deep learning, full-stack engineering, and system design. 
         </p>
         <p>
           This site is still in development,
-          but here I'll be posting my personal projects and assorted thoughts on technology in addition to other things I'm interested in.
+          but here I&apos;ll be posting my personal projects and my thoughts on current technology, in addition to other things I&apos;m interested in.
         </p>
         <p class='mb-4 text-justify'>
           Some of my most recent experiences involve developing and optimizing recursive machine learning models for demand forecasting and supply chain management, as well as deploying
-          full-stack cloud applications that leverage the power of open-source LLM's.
+          full-stack cloud applications that leverage the power of open-source LLM&apos;s.
         </p>
         <p class='mb-4 text-center'>
           Please direct all inquiries to anshosca@gmail.com.
